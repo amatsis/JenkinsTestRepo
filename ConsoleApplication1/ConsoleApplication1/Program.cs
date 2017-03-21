ec91c965c262a34c3074fe491b4c90698599371f
amatsis <amatsis@privateclientresources.net>
2017-03-21 11:15:38 -0400

    commit3

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            //test2
+            //test3
             Console.WriteLine("Test1");
             Console.Read();
         }
