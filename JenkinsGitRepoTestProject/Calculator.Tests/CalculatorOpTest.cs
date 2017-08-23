dd00557a90d2f3f04ae0739a1ac0dab050392e5e
amatsis <amatsis@privateclientresources.net>
2017-08-23 09:56:45 -0400

    unit test changes

---
+++ JenkinsGitRepoTestProject/Calculator.Tests/CalculatorOpTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator;
+
+
+namespace Calculator.Tests
+{
+    [TestClass]
+  public class CalculatorOpTest
+    {
+        [TestMethod]
+        public void TestingAdd()
+        {
+            //Arranging the test
+            CalculatorOP sut = new CalculatorOP();
+            //Act
+            int result = sut.Add(5,4);
+            //Assert 
+            Assert.AreEqual(9,result);
+        }
+        [TestMethod]
+        public void TestingMul()
+        {
+            //Arranging the test
+            CalculatorOP sut = new CalculatorOP();
+            //Act
+            int result = sut.mul(5, 4);
+            //Assert 
+            Assert.AreEqual(20, result);
+        }
+    }
+}
